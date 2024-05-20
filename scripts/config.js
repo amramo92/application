75133ba94788501cd3be9db5c478b6ec90336391
Amra <167887443+amramo92@users.noreply.github.com>
2024-05-20 14:46:44 +0200

    Add files via upload

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Chat", "Tigre", "Guépard"]
+const listePhrases = ["Tout ira bien !", "La vie, l'univers et le bonheur", "Merci et à bientôt"]
